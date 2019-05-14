5585d21265e662b96d351a374c10b68f7ee8d594
HeroStatic <1069393525@qq.com>
2019-05-15 00:15:08 +0800

    1111

---
+++ src/store/modules/todos.js
@@ -0,0 +1,26 @@
+const state = {
+    todos:[
+        {list:"打场高尔夫",hasfinished:true},
+        {list:"会见奥巴马",hasfinished:false},
+        {list:"回家敲代码",hasfinished:false},
+        {list:"回家吃饭",hasfinished:true}
+    ]
+}
+const getters = {
+
+}
+
+const mutations = {
+    
+}
+const actions = {
+    
+}
+
+export default {
+    namespaced:true,
+    state,
+    actions,
+    mutations,
+    getters
+}
